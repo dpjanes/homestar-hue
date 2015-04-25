e4dee30a432fd02d2566ad572b907bbc9566d06d
David Janes <davidjanes@davidjanes.com>
2015-04-25 10:49:15 -0400

    remove old documentation dups

--- HueLightBridge.js
+++ HueLightBridge.js
@@ -42,16 +42,10 @@ var OFFSET_PULL = 200000;
 
 
 /**
- *  EXEMPLAR and INSTANCE
+ *  See {iotdb.bridge.Bridge#Bridge} for documentation.
  *  <p>
- *  No subclassing needed! The following functions are
- *  injected _after_ this is created, and before .discover and .connect
- *  <ul>
- *  <li><code>discovered</code> - tell IOTDB that we're talking to a new Thing
- *  <li><code>pulled</code> - got new data
- *  <li><code>connected</code> - this is connected to a Thing
- *  <li><code>disconnnected</code> - this has been disconnected from a Thing
- *  </ul>
+ *  @param {object|undefined} native
+ *  only used for instances, should be 
  */
 var HueLightBridge = function (initd, native) {
     var self = this;
@@ -84,13 +78,7 @@ HueLightBridge.prototype.name = function () {
 /* --- lifecycle --- */
 
 /**
- *  EXEMPLAR.
- *  Discover Hue
- *  <ul>
- *  <li>look for Things (using <code>self.bridge</code> data to initialize)
- *  <li>find / create a <code>native</code> that does the talking
- *  <li>create an HueLightBridge(native)
- *  <li>call <code>self.discovered(bridge)</code> with it
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 HueLightBridge.prototype.discover = function () {
     var self = this;
@@ -169,8 +157,7 @@ HueLightBridge.prototype._discover_native = function (native) {
 };
 
 /**
- *  INSTANCE
- *  This is called when the Bridge is no longer needed. When
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 HueLightBridge.prototype.connect = function (connectd) {
     var self = this;
@@ -213,8 +200,7 @@ HueLightBridge.prototype._forget = function () {
 };
 
 /**
- *  INSTANCE and EXEMPLAR (during shutdown).
- *  This is called when the Bridge is no longer needed.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 HueLightBridge.prototype.disconnect = function () {
     var self = this;
@@ -228,8 +214,7 @@ HueLightBridge.prototype.disconnect = function () {
 /* --- data --- */
 
 /**
- *  INSTANCE.
- *  Send data to whatever you're taking to.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 HueLightBridge.prototype.push = function (pushd) {
     var self = this;
@@ -300,9 +285,7 @@ HueLightBridge.prototype.push = function (pushd) {
 };
 
 /**
- *  INSTANCE.
- *  Pull data from whatever we're talking to. You don't
- *  have to implement this if it doesn't make sense
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 HueLightBridge.prototype.pull = function () {
     var self = this;
@@ -377,18 +360,7 @@ HueLightBridge.prototype.pull = function () {
 /* --- state --- */
 
 /**
- *  INSTANCE.
- *  Return the metadata - compact form can be used.
- *  Does not have to work when not reachable
- *  <p>
- *  Really really useful things are:
- *  <ul>
- *  <li><code>iot:thing</code> required - a unique ID
- *  <li><code>iot:device</code> suggested if linking multiple things together
- *  <li><code>schema:name</code>
- *  <li><code>iot:number</code>
- *  <li><code>schema:manufacturer</code>
- *  <li><code>schema:model</code>
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 HueLightBridge.prototype.meta = function () {
     var self = this;
@@ -407,10 +379,7 @@ HueLightBridge.prototype.meta = function () {
 };
 
 /**
- *  INSTANCE.
- *  Return True if this is reachable. You
- *  do not need to worry about connect / disconnect /
- *  shutdown states, they will be always checked first.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 HueLightBridge.prototype.reachable = function () {
     return this.native !== null;
