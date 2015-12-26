23eac0ec6087ea1c5fc1bd1442f06c4cccae928a
David Janes <davidjanes@davidjanes.com>
2015-12-26 09:22:14 -0330

    move to IoTQL model

--- models/HueLight.js
+++ models/HueLight.js
@@ -8,15 +8,7 @@
 
 var iotdb = require("iotdb");
 
-exports.Model = iotdb.make_model('HueLight')
-    .facet(":lighting")
-    .name("Hue Light")
-    .description("Philips Hue colored light")
-    .io("on", iotdb.boolean.on)
-    .io("color", iotdb.string.color)
-    .make();
-
 exports.binding = {
     bridge: require('../HueLightBridge').Bridge,
-    model: exports.Model,
+    model: require('./HueLight.json'),
 };
